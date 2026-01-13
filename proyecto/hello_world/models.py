8bb02513894194fb05dbf079b2013a61c6ff8966
juanch0de <juan.henao6@utp.edu.co>
2026-01-13 14:50:30 -0500

    Database created with entity Producto

--- proyecto/hello_world/models.py
+++ proyecto/hello_world/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=100)
+    precio = models.IntegerField()
+    cantidad = models.IntegerField()
