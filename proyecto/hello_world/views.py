d84be84dde6eaecbba5f5c5f11ed1c918fe455c5
juanch0de <juan.henao6@utp.edu.co>
2026-01-14 10:59:23 -0500

    Added JSON page productos

--- proyecto/hello_world/views.py
+++ proyecto/hello_world/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+from .models import Producto
 
 def hello_world(request, name):
     return HttpResponse(f"Hello World! Qhubo {name}!!!")
 
+def lista_productos(request):
+    productos = Producto.objects.all()
+
+    data = [
+        {
+            "codigo": p.codigo,
+            "nombre": p.nombre,
+            "descripcion": p.descripcion,
+            "precio": p.precio,
+            "cantidad": p.cantidad,
+        }
+        for p in productos
+    ]
+
+    return JsonResponse(data, safe=False)
