2ef8fe1d207dbe8df757a9f951fd5048a5430371
juanch0de <juan.henao6@utp.edu.co>
2026-01-28 17:53:45 -0500

    First post method performed

--- proyecto/hello_world/views.py
+++ proyecto/hello_world/views.py
@@ -1,28 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import Producto
+import json
 
 def hello_world(request, name):
     return HttpResponse(f"Hello World! Qhubo {name}!!!")
 
-def lista_productos(request):
-    productos = Producto.objects.all()
+@csrf_exempt
+def productos(request):
+    if request.method == "GET":
+        productos = Producto.objects.all()
 
-    data = [
-        {
-            "id": p.id,
-            "codigo": p.codigo,
-            "nombre": p.nombre,
-            "descripcion": p.descripcion,
-            "precio": p.precio,
-            "cantidad": p.cantidad,
-        }
-        for p in productos
-    ]
+        data = [
+            {
+                "id": p.id,
+                "codigo": p.codigo,
+                "nombre": p.nombre,
+                "descripcion": p.descripcion,
+                "precio": p.precio,
+                "cantidad": p.cantidad,
+            }
+            for p in productos
+        ]
+        return JsonResponse(data, safe = False)
 
-    return JsonResponse(data, safe=False)
+    elif request.method == "POST":
+        body = json.loads(request.body)
 
+        producto = Producto.objects.create(
+                codigo = body["codigo"],
+                nombre = body["nombre"],
+                descripcion = body["descripcion"],
+                precio = body["precio"],
+                cantidad = body["cantidad"],
+                )
+        return JsonResponse(
+                {"id": producto.id},
+                status = 201
+                )
+    
 def producto_individual(request, identificador):
     try:
         if identificador.isdigit():
